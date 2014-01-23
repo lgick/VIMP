86ab3eece227499a025d8ffeb553b90a50387dfe
lgick <lgick@yandex.ru>
2014-01-23 18:18:35 +0400

    обновление views/ & routes/

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
-exports.index = function (req, res){
-  res.render('index', {
+exports.index = function (req, res) {
+  res.render('vimp', {
     baseURL: '/',
     jsURL: '/javascripts/',
     cssURL: '/stylesheets/',
